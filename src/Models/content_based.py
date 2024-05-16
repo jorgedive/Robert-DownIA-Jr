aa14692aadc5de9ebc68ee755332465a0ce9c0ca
JavExile <jfis.exilaltreon@gmail.com>
2024-05-16 15:49:36 +0200

    Adds EDA02, content_based model, demographic model

---
+++ src/Models/content_based.py
@@ -0,0 +1,186 @@
+import os
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+import re
+from dotenv import load_dotenv
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+import pickle
+import numpy as np
+import pandas as pd
+
+nltk.download("stopwords")
+nltk.download("punkt")
+
+load_dotenv()
+models_path = os.getenv("MODELS_PATH")
+files_path = os.getenv("FILES_LOCATION")
+
+RECOMMENDER_TYPE = "content_based"
+MODEL = "tfidf_description_matrix.pickle"
+FILE_DATA = "cleaned_content_based.csv"
+
+default_model_path = os.path.join(models_path, RECOMMENDER_TYPE, MODEL)
+default_files_path = os.path.join(files_path, "CSV", FILE_DATA)
+
+
+def load_data(path=default_files_path):
+    try:
+        df = pd.read_csv(path, low_memory=False)
+        return df
+    except Exception as e:
+        print(f"Could not retrieve the file. Error: {e}")
+        return None
+
+
+def save_param_matrix(param_matrix, path=default_model_path):
+    """Saves parameter matrix
+    Args:
+        param_matrix (numpy ndarray): numpy matrix containing similarity scores between movies
+        path (str): path where the model will be saved with pickle
+    """
+    with open(path, "wb") as f:
+        pickle.dump(param_matrix, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_param_matrix(path=default_model_path):
+    """Loads the model
+    Args:
+        path (str): path where the model is located
+
+    Returns:
+        param_matrix (numpy ndarray): numpy matrix containing the similarity for each movie
+    """
+
+    with open(path, "rb") as f:
+        param_matrix = pickle.load(f)
+    return param_matrix
+
+
+def preprocess_data(dataframe, col="description"):
+    """Preprocess data before training the recommender based on description.
+    Args:
+        dataframe (pandas DataFrame): pandas DataFrame containing the data
+        col (str): number of the column to process. Default "description"
+
+    Returns:
+        dataframe (pandas DataFrame): processed data with nltk
+    """
+
+    nltk.download("stopwords")
+    stop_w = set(stopwords.words("english"))
+
+    remove_non_alpha_fn = lambda x: re.sub(r"[^a-zA-Z]", " ", x)
+    tokenize_fn = lambda x: nltk.tokenize.word_tokenize(x)
+
+    stemmer = SnowballStemmer("english")
+    stemmer_fn = lambda desc: " ".join([stemmer.stem(w) for w in desc if w.lower() not in stop_w])
+
+    for fn in (remove_non_alpha_fn, tokenize_fn, stemmer_fn):
+        dataframe[col] = dataframe[col].transform(func=fn)
+    return dataframe
+
+
+def train_content_recommender(dataframe, preprocess_fn=preprocess_data, col="description", count_vec=False):
+    """Recommender training
+    Args:
+        dataframe (pandas DataFrame): pandas DataFrame with the training data
+        preprocess_fn (function): function that processes the data before training
+        col (str): column where we perform the training. Default "description"
+        count_vec (bool): boolean to indicate if use CountVectorizer for metadata column. Default False
+
+    Returns:
+        param_matrix (numpy ndarray): numpy matrix containing the the similarities between movies
+    """
+
+    if count_vec:
+        dataframe[col] = dataframe[col].fillna("")
+        count_vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0., stop_words="english")
+        count_vec_matrix = count_vectorizer.fit_transform(dataframe[col])
+        return cosine_similarity(count_vec_matrix, count_vec_matrix)
+
+    processed_data = preprocess_fn(dataframe, col)
+    tfidf = TfidfVectorizer(analyzer="word", ngram_range=(1, 2), stop_words="english", min_df=0.)
+    tfidf_matrix = tfidf.fit_transform(processed_data[col])
+
+    return linear_kernel(tfidf_matrix, tfidf_matrix)
+
+
+def get_recommendation(movie, top_n=10, preprocess_fn=preprocess_data, col="description", count_vec=False,
+                                   file_path=default_files_path, model_path=default_model_path):
+    """Recommender based on description or movie metadata
+    Args:
+        movie (str): movie title to take as baseline for the recommendations
+        top_n (int): number of movies that will be shown. Default 10
+        preprocess_fn (function): function to preprocess the data in case there is no model
+        col (str): name for the column to train on in case training is needed. Default description
+        count_vec (bool): boolean to indicate if use CountVectorizer. Default False
+        file_path (str): path where the csv data file is located
+        model_path (str): path where the model is stored
+
+    Returns:
+        movies_list (list): list with the recommended movies
+    """
+
+    df = load_data(file_path)
+    if df is None:
+        raise ValueError("Dataframe not provided.")
+
+    if not (isinstance(movie, str)) and (movie not in df["title"].unique()):
+        raise ValueError("Movie not found.")
+
+    try:
+        param_matrix = load_param_matrix(model_path)
+    except Exception:
+        param_matrix = train_content_recommender(df, preprocess_fn, col, count_vec)
+        save_param_matrix(param_matrix, model_path)
+
+    mapper = pd.Series(df.index, index=df["title"]).drop_duplicates()
+    scores = np.argsort(param_matrix[mapper[movie]])[::-1]
+    movie_indices = scores[1:top_n + 1]
+
+    movies_list = df["title"].iloc[movie_indices].to_list()
+    return movies_list
+
+
+def improved_recommendations(movie, q=0.6, top_n=25, preprocess_fn=preprocess_data, col="description", count_vec=False,
+                             file_path=default_files_path, model_path=default_model_path):
+    """Recommender based on description or movie metadata
+    Args:
+        movie (str): movie title to take as baseline for the recommendations
+        q (float): quantile to perform the filtering on the top q movies. Default 0.6
+        top_n (int): number of movies to filter on with the percentile. Default 25
+        preprocess_fn (function): function to preprocess the data in case there is no model
+        col (str): name for the column to train on in case training is needed. Default description
+        count_vec (bool): boolean to indicate if use CountVectorizer. Default False
+        file_path (str): path where the csv data file is located
+        model_path (str): path where the model is stored
+
+    Returns:
+        movies_list (list): list with the recommended movies
+    """
+
+    if (q <= 0) or (q >= 1):
+        raise ValueError("Quantile value not valid.")
+
+    try:
+        assert top_n >= 1
+    except AssertionError as e:
+        print(f"Number of movies must be positive. Error: {e}")
+
+    recommended_movies = get_recommendation(movie, top_n, preprocess_fn, col, count_vec, file_path, model_path)
+    df = load_data(file_path)
+    recommended_df = df[df["title"].isin(recommended_movies)]
+
+    def wr_func(data, C, m):
+        v = data["vote_count"]
+        R = data["vote_average"]
+        return (v / (v + m) * R) + (m / (v + m) * C)
+
+    C = recommended_df["vote_average"].mean()
+    m = recommended_df["vote_count"].quantile(q)
+    recommended_df = recommended_df[recommended_df["vote_count"] > m]
+    recommended_df["weighted_score"] = recommended_df.apply(wr_func, axis=1, C=C, m=m)
+
+    return recommended_df.sort_values(by="weighted_score", ascending=False)["title"].to_list()
---
+++ src/Models/demographic_filtering.py
@@ -0,0 +1,70 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+import ast
+
+load_dotenv()
+data_path = os.getenv("FILES_LOCATION")
+
+# A Files/CSV/cleaned_movies.csv must exist within the Files folder
+FILENAME = "cleaned_movies.csv"
+
+
+def load_data(path=data_path):
+    try:
+        df = pd.read_csv(os.path.join(path, "CSV", FILENAME), low_memory=False)
+        return df
+    except Exception as e:
+        print(f"Could not retrieve the file. Error: {e}")
+        return None
+
+
+def get_demographic_recommendation(genre, path=data_path, q=0.95, n_movies=10):
+    """Simple recommender system that provides the top rated movies for a given genre.
+        Args:
+            genre (str): selected genre to perform the filtering
+            path (str): path where the file to load as a dataframe is located
+            q (float): q quantile to perform the filtering on the top q movies. Default 0.95
+            n_movies (int): number of movies to show to the user
+
+        Returns:
+            movies_list (list): list with the recommended movies
+        """
+
+    df = load_data(path)
+    if df is None:
+        raise ValueError("Dataframe not provided.")
+
+    # Gets the unique genres in the set to perform input validation
+    unique_genres = set([genre for val in df["genres"].values for genre in ast.literal_eval(val)])
+
+    if not (isinstance(genre, str)) and (genre.capitalize() in unique_genres):
+        raise ValueError("Valid genre not provided.")
+
+    if (q <= 0) or (q >= 1):
+        raise ValueError("Quantile value not valid.")
+
+    try:
+        assert n_movies >= 1
+    except AssertionError as e:
+        print(f"Number of movies must be positive. Error: {e}")
+
+    genre = genre.capitalize()
+    df["genres"] = df["genres"].apply(lambda x: ast.literal_eval(x))  # Evaluate entries as lists
+    df["tmp_mask"] = df["genres"].apply(lambda x: genre in x)  # Mask to filter entries with the specified genre
+    df_genre = df[df["tmp_mask"]]  # Filtered df by the mask
+
+    # Weighted rate from IMDB
+    def wr_func(data, C, m):
+        v = data["vote_count"]
+        R = data["vote_average"]
+        return (v / (v + m) * R) + (m / (v + m) * C)
+
+    # Compute the weighted rating for the recommender
+    C = df_genre["vote_average"].mean()
+    m = df_genre["vote_count"].quantile(q)
+    df_genre = df_genre[df_genre["vote_count"] > m]
+    df_genre["weighted_score"] = df_genre.apply(wr_func, axis=1, C=C, m=m)
+
+    movies_list = df_genre.sort_values("weighted_score", ascending=False)["title"].to_list()[:n_movies]
+    return movies_list
