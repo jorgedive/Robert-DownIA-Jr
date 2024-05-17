14300fb9c5edd8fd68e5d40662aee5d30a772494
JavExile <jfis.exilaltreon@gmail.com>
2024-05-17 12:33:19 +0200

    Adds collaborative_filtering model. Adds muffin_vs_chihuahua model

--- src/Models/muffin_vs_chihuahua_model.py
+++ src/Models/muffin_vs_chihuahua_model.py
@@ -1,19 +1,19 @@
 import os
-
+import tensorflow as tf
 from dotenv import load_dotenv
 from kaggle.api.kaggle_api_extended import KaggleApi
-from keras.src.layers import (Conv2D, Flatten, MaxPooling2D, RandomFlip, RandomRotation, RandomZoom, Rescaling,
-                              Resizing)
-from keras.src.optimizers import Adam
-from tensorflow.keras import Input, Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import Sequential
 from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.callbacks import ModelCheckpoint
+
 
 load_dotenv()
 
-
 def download_kaggle_dataset(dataset_name, files_path):
-    kaggle_path = os.path.join(files_path, "PNG", 'muffin-chihuahua')
+    kaggle_path = os.path.join(files_path, "PNG", "muffin-chihuahua")
+
     if not os.path.exists(kaggle_path):
         api_kaggle = KaggleApi()
         api_kaggle.authenticate()
@@ -21,52 +21,60 @@ def download_kaggle_dataset(dataset_name, files_path):
     return kaggle_path
 
 
-def main():
+def get_datasets(random_state=42):
     files_path = os.getenv("FILES_LOCATION")
-    download_kaggle_dataset(os.getenv("KAGGLE_CHIHUAHUA"), files_path)
+    images_path = os.path.join(files_path, "PNG", "muffin_chihuahua")
 
-    images_path = os.path.join(files_path, "PNG", "muffin-chihuahua")
+    train_ds = image_dataset_from_directory(
+        os.path.join(images_path, "train"),
+        label_mode="binary",
+        batch_size=32,
+        seed=random_state,
+        interpolation="bicubic")
 
-    train_ds = image_dataset_from_directory(images_path,
-                                            label_mode="binary",
-                                            batch_size=16,
-                                            validation_split=0.2,
-                                            subset="training",
-                                            seed=42,
-                                            interpolation="bicubic")
-    val_ds = image_dataset_from_directory(images_path,
-                                          label_mode="binary",
-                                          batch_size=16,
-                                          validation_split=0.2,
-                                          subset="validation",
-                                          seed=42,
-                                          interpolation="bicubic")
+    val_ds = image_dataset_from_directory(
+        os.path.join(images_path, "test"),
+        label_mode="binary",
+        batch_size=32,
+        seed=random_state,
+        interpolation="bicubic")
 
-    resize_and_rescale = Sequential([Resizing(128, 128), Rescaling(1. / 255)])
-    augment_image = Sequential([RandomFlip("horizontal"), RandomRotation(0.2),
-                                RandomZoom(height_factor=(0, 0.2), width_factor=(0, 0.2))])
+    return train_ds, val_ds
+
+
+def train_model(epochs=50, random_state=42):
+    tf.random.set_seed(random_state)
+    files_path = os.getenv("FILES_LOCATION")
+
+    try:
+        train_ds, val_ds = get_datasets(random_state)
+    except:
+        download_kaggle_dataset(os.getenv("KAGGLE_CHIHUAHUA"), files_path)
+        train_ds, val_ds = get_datasets(random_state)
+
+    resize_and_rescale = Sequential([layers.Resizing(128, 128), layers.Rescaling(1. / 255)])
+    augment_image = Sequential([layers.RandomFlip("horizontal"), layers.RandomRotation(0.2)])
 
     model = Sequential([resize_and_rescale,
                         augment_image,
-                        Input((128, 128, 3)),
-                        Conv2D(16, (3, 3), strides=(2, 2), activation='relu', kernel_initializer="he_normal"),
-                        Conv2D(32, (3, 3), strides=(2, 2), activation='relu', kernel_initializer="he_normal"),
-                        MaxPooling2D((2, 2)),
-                        Conv2D(32, (3, 3), activation='relu', kernel_initializer="he_normal"),
-                        MaxPooling2D((2, 2)),
-                        Flatten(),
-                        Dense(128, activation='relu', kernel_initializer="he_normal"),
-                        Dense(1, activation='sigmoid')])
+                        layers.Conv2D(16, (3, 3), activation="relu", kernel_initializer="he_normal"),
+                        layers.MaxPooling2D((2, 2)),
+                        layers.Conv2D(32, (3, 3), activation="relu", kernel_initializer="he_normal"),
+                        layers.MaxPooling2D((2, 2)),
+                        layers.Conv2D(32, (3, 3), activation="relu", kernel_initializer="he_normal"),
+                        layers.Conv2D(64, (3, 3), activation="relu", kernel_initializer="he_normal"),
+                        layers.MaxPooling2D((2, 2)),
+                        layers.Flatten(),
+                        layers.Dropout(rate=0.3),
+                        layers.Dense(128, activation="relu", kernel_initializer="he_normal"),
+                        layers.Dense(1, activation="sigmoid")])
 
-    # RMSprop(momentum=0.9, decay=0.01)
-    model.compile(optimizer=Adam(decay=0.01), loss='binary_crossentropy', metrics=['accuracy'])
-    model.fit(train_ds, validation_data=val_ds, epochs=10)
+    check_cb = ModelCheckpoint(filepath=os.path.join(os.getenv("MODELS_PATH"), "muffin_chihuahua",
+                                                                        "model_{epoch:04d}.keras"),
+                                                  monitor="val_accuracy",
+                                                  mode="max",
+                                                  save_best_only=True)
 
-    precision = model.evaluate(val_ds)
-    print(f"Model precision = {precision[1] * 100}%. ")
-
-    model.save(os.getenv("MODELS_PATH"))
-
-
-if __name__ == "__main__":
-    main()
+    model.compile(optimizer=Adam(learning_rate=1e-3), loss="binary_crossentropy", metrics=["accuracy"])
+    model.fit(train_ds, validation_data=val_ds, epochs=epochs, callbacks=[check_cb])
+    return model
